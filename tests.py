4f0f66b9a6d7b584008b7e8be59a99a327c010b7
doriable <doria.keung@gmail.com>
2016-02-24 23:21:24 -0800

    Testing work in progress.

--- tests.py
+++ tests.py
@@ -6,7 +6,6 @@ from unittest import TestCase, main
 # Import Tornado Testing
 from tornado.testing import AsyncHTTPTestCase
 from tornado.websocket import websocket_connect
-from flask_sqlalchemy import SQLAlchemy
 import tornado.web
 
 # Import Server
@@ -14,7 +13,7 @@ from server import *
 
 
 ################################################################################
-### (1) Mock Database Setup
+### (1) Connect to Test Database
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///office-jukebox-test'
 app.config['TESTING'] = True
@@ -38,16 +37,14 @@ class WebSocketsTest(AsyncHTTPTestCase):
     @tornado.testing.gen_test
     def test_websocket_first_load(self):
 
-        print "####################"
         # self.get_http_port() gives the port of the running test server
         ws_url = "ws://localhost:" + str(self.get_http_port()) + "/websocket/"
-        print ws_url
 
         # The ws_url feeds into our WebSocke client
         ws_client = yield websocket_connect(ws_url)
 
         # running a test on the WebSocket
-        first_load_msg = '{"jukebox_id" : "a49ec47d-5808-4975-bfcc-b42d2667a9a5" ,"first_load" : "yes"}'
+        first_load_msg = '{"jukebox_id" : "a49ec47d-5808-4975-bfcc-b42d2667a9a5","first_load" : "yes"}'
         ws_client.write_message(first_load_msg)
         response = yield ws_client.read_message()
         self.assertEqual(response, first_load_msg)
@@ -59,38 +56,50 @@ class WebSocketsTest(AsyncHTTPTestCase):
 
 class OfficeJukeboxTest(TestCase):
     """Testing Office Jukebox routes."""
-    pass
 
     def setUp(self):
         """Runs on test case setup."""
 
-        # Mock database
-        # Connect to mock database
-        # Create all tables in mock db
-        pass
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
     def tearDown(self):
         """Runs when test case ends."""
 
-        # End mock database session
-        # Drop all database tables in mock db
         pass
+
+    def test_jukebox_homepage(self):
+        """Ensure homepage is properly rendered."""
+
+        result = self.client.get("/")
+        self.assertEqual(result.status_code, 200)
 
     def test_jukebox_creation(self):
         """Ensure that jukebox is made """
-        pass
+
+        result = self.client.get("/jukebox")
+        self.assertEqual(result.status_code, 302)
+        # need to do .post -- currently returning 405
 
     def test_guest_creation(self):
+        """Ensure that guest view is rendered and guest is created."""
+
+        result = self.client.get("/guest")
+        self.assertEqual(result.status_code, 302)
+        # need to do .post -- currently returning 405
+        # TODO: add condition for guest creation in database
+
+    def test_song_creation(self):
+        """Ensure song is created and added to database."""
+
+        # TODO: set up a song object, query and check condition
         pass
 
-    def test_admin_creation(self):
-        pass
+    def test_render_goodbye(self):
+        """Ensure goodbye page renders."""
 
-    def test_jukebox_id_get(self):
-        pass
-
-    def test_guest_id_get(self):
-        pass
+        result = self.client.get("/goodbye")
+        self.assertIn("<h1>Bye now, have a nice day!</h1>", result.data)
 
 if __name__ == "__main__":
     main()
